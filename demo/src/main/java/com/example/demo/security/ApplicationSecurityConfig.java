8470a7d7cb18d6525dc9666054f1fb7b772104f2
prajul-shinde <prajulshinde@gmail.com>
2021-03-16 13:04:06 +0530

    2-Role based Basic InMemory authentication

--- demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,20 +1,38 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.example.demo.security.ApplicationUserRole.ADMIN;
+import static com.example.demo.security.ApplicationUserRole.STUDENT;
 
 //form base to basic auth
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+// InMemoryAuth: wont able to login without injecting it
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","index","/css/*","/js/*")
-                .permitAll()
+                .antMatchers("/","index","/css/*","/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -23,5 +41,23 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 //                             :-disadvantage cannot logout
 //                             :- select basic auth in postman and send user as username and generated password as
 //                                generated from console
+    }
+       //InMemoryUserDetailsManager for inMemory authentication
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name())   //ROLE_STUDENT
+                .build();
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMIN.name())
+                .build();
+        return new InMemoryUserDetailsManager(
+                annaSmithUser,lindaUser
+        );
     }
 }
